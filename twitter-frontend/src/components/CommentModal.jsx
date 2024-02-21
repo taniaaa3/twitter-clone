5883d94936e699b18cc82cac1b4a2d483b5db6ae
Taniya Darwani <taniyadarwani3@gmail.com>
2024-02-21 18:52:31 +0530

    fifth commit

--- twitter-frontend/src/components/CommentModal.jsx
+++ twitter-frontend/src/components/CommentModal.jsx
@@ -29,7 +29,7 @@ const CommentModal = (props) => {
   return (
     <div>
       {/* <!-- Button trigger modal --> */}
-      <TERipple rippleColor="white">
+      <TERipple>
         <div onClick={(e) => {setShowModal(true); e.stopPropagation(); setID(props.val._id)}} className='mr-2 hover:bg-blue-300 cursor-pointer px-2 py-1 flex flex-row items-center'>
                     <i className='fa-regular text-blue-500 fa-message m-1'></i>
                     <p>{props.val.replies ? props.val.replies.length : "0"}</p>
@@ -74,7 +74,7 @@ const CommentModal = (props) => {
               <textarea name="" id="" cols="30" rows="10" className="border p-2 border-2 resize-none block" onChange={(e)=>{setContent(e.target.value)}} value={content}></textarea>
             </TEModalBody>
             <TEModalFooter>
-              <TERipple rippleColor="light">
+              <TERipple>
                 <button
                   type="button"
                   className="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
@@ -83,7 +83,7 @@ const CommentModal = (props) => {
                   Close
                 </button>
               </TERipple>
-              <TERipple rippleColor="light">
+              <TERipple>
                 <button
                   type="submit"
                   className="ml-1 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
--- twitter-frontend/src/components/EditProfileModal.jsx
+++ twitter-frontend/src/components/EditProfileModal.jsx
@@ -33,7 +33,7 @@ const EditProfileModal = (props) => {
   return (
     <div>
       {/* <!-- Button trigger modal --> */}
-      <TERipple rippleColor="white">
+      <TERipple>
         <button onClick={() => setShowModal(true)} className='px-9 sm:px-4 py-2 border border-black m-3'>Edit</button>
       </TERipple>
 
@@ -88,7 +88,7 @@ const EditProfileModal = (props) => {
               
             </TEModalBody>
             <TEModalFooter>
-              <TERipple rippleColor="light">
+              <TERipple>
                 <button
                   type="button"
                   className="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
@@ -98,7 +98,7 @@ const EditProfileModal = (props) => {
                   Close
                 </button>
               </TERipple>
-              <TERipple rippleColor="light">
+              <TERipple>
                 <button
                   type="submit"
                   className="ml-1 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
